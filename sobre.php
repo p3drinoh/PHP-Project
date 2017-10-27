e8e21a35a622b5006c4be0bfa5f7ad7e28b73512
P3drinoh <p3drinoh@gmail.com>
2017-10-27 10:18:30 -0200

    Programa Inicial de PHP a ser incrementado

---
+++ sobre.php
@@ -0,0 +1,3 @@
+<?php include("cabecalho.php"); ?>
+<h1>Essa é pequeno programa feito para mostrar a familiariadade de um programador com o PHP, o HTML e o CSS. Será incrementado e modificado aos poucos.</h1>
+<?php include("rodape.php"); ?>
